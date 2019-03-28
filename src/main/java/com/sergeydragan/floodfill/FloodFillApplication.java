6f44c39638f717618010cd69e2630b6cb9604367
sdrahan <serhii.drahan@deposit-solutions.com>
2019-03-28 21:27:34 +0100

    Visualise elements.

--- src/main/java/com/sergeydragan/floodfill/FloodFillApplication.java
+++ src/main/java/com/sergeydragan/floodfill/FloodFillApplication.java
@@ -1,9 +1,24 @@
 package com.sergeydragan.floodfill;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FloodFillApplication {
 
     public static void main(String[] args) {
+        List<List<String>> field = new ArrayList<>();
+        field.add(new ArrayList<>(Arrays.asList("0", "0", "0", "0", "0")));
+        field.add(new ArrayList<>(Arrays.asList("1", "1", "0", "0", "0")));
+        field.add(new ArrayList<>(Arrays.asList("0", "1", "0", "1", "0")));
+        field.add(new ArrayList<>(Arrays.asList("0", "1", "1", "1", "0")));
+        field.add(new ArrayList<>(Arrays.asList("0", "0", "0", "0", "0")));
 
+        final FloodFiller floodFiller = new SimpleFloodFiller();
+        floodFiller.fillField(field);
+
+        // ▓
+        // ░
     }
 }
 
--- src/main/java/com/sergeydragan/floodfill/SimpleFloodFiller.java
+++ src/main/java/com/sergeydragan/floodfill/SimpleFloodFiller.java
@@ -6,6 +6,27 @@ public class SimpleFloodFiller implements FloodFiller {
 
     @Override
     public List<List<String>> fillField(List<List<String>> field) {
+        String visualisation = "";
+        for (List<String> aField : field) {
+            for (String anAField : aField) {
+                visualisation = visualisation.concat(cellToChar(anAField)).concat(" ");
+            }
+            visualisation = visualisation.concat("\n");
+        }
+        System.out.println(visualisation);
+
         return null;
+    }
+
+    private String cellToChar(String cellValue) {
+        switch (cellValue) {
+            case "0":
+                return "░";
+            case "1":
+                return "▓";
+            case "*":
+                return "*";
+        }
+        return "?";
     }
 }
